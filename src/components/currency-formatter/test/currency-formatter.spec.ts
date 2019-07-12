7b560a13ccd683626e863d0fd37356b32d19a979
Lucas de Farias <defarias.lucas@gmail.com>
2019-07-12 09:05:08 +0200

    Fix test by manually mocking Intl.NumberFormat class

--- src/components/currency-formatter/test/currency-formatter.spec.ts
+++ src/components/currency-formatter/test/currency-formatter.spec.ts
@@ -1,15 +1,26 @@
 import { CurrencyFormatter } from '../currency-formatter';
 
-it('should display 1.214.300,02€ when given 1214300.02 EUR', () => {
-  const formatter = new CurrencyFormatter();
-  formatter.locale = 'es-ES';
-  formatter.currencyAmount = {
-    currency: 'EUR',
-    amount: '1214300.02'
-  };
+class FakeNumberFormat extends Intl.NumberFormat {
+  format(num: number) {
+    return '1.214.300,02 €';
+  }
+}
 
-  const returnValue = formatter.formattedCurrency();
-  // Test fails because node by default only ships with a limited set of locales
-  // See: https://stackoverflow.com/questions/49052731/jest-test-intl-datetimeformat/50119588
-  expect(returnValue).toBe('1.214.300,02€');
+describe('CurrencyFormatter', () => {
+
+  it('should display 1.214.300,02 € when given 1214300.02 EUR', () => {
+    // Node by default only ships with a limited set of locales
+    Intl.NumberFormat = <any> FakeNumberFormat;
+
+    const formatter = new CurrencyFormatter();
+    formatter.locale = 'es-ES';
+    formatter.currencyAmount = {
+      currency: 'EUR',
+      amount: '1214300.02'
+    };
+
+    const returnValue = formatter.formattedCurrency();
+
+    expect(returnValue).toBe('1.214.300,02 €');
+  });
 });
